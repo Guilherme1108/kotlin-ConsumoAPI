dc5e863c505512346716e9dd6779b48885658f1d
Guilherme Moreira <guilherme.moreira1108@gmail.com>
2026-04-06 11:28:54 -0300

    fazendo busca pelo CEP

---
+++ app/src/main/java/com/aulasandroid/consumoapi/service/RetrofitFactory.kt
@@ -0,0 +1,18 @@
+package com.aulasandroid.consumoapi.service
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
+class RetrofitFactory {
+    val BASE_URL = "https://viacep.com.br/ws/"
+    private val retrofitFactory = Retrofit
+        .Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .build()
+
+    fun getEnderecoService(): EnderecoService {
+        return retrofitFactory.create(EnderecoService::class.java)
+    }
+}
